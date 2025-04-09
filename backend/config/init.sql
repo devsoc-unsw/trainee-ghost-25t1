7a7d9664e8ec4b1ff8f5a8174abf3ccc9e6ea3ad
William Millet <57897661+WilliamMillet@users.noreply.github.com>
2025-04-10 06:37:50 +1000

    Fixed errors in the initialisaton SQL query

--- backend/config/init.sql
+++ backend/config/init.sql
@@ -8,9 +8,8 @@ CREATE TABLE IF NOT EXISTS users (
    email VARCHAR(255) NOT NULL UNIQUE,
    hashed_password VARCHAR(255) NOT NULL,
    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-   team_id INT,
-   FOREIGN KEY (team_id) REFERENCES teams(id) ON DELETE CASCADE
-)
+   team_id INT
+);
 
 CREATE TABLE IF NOT EXISTS teams (
     id INT AUTO_INCREMENT PRIMARY_KEY,
@@ -30,7 +29,15 @@ CREATE TABLE IF NOT EXISTS teams (
     -- are currently overdue, so we do not need to store anything here. This 
     -- will simplify things
     FOREIGN KEY (admin_user_id) REFERENCES users(id) ON DELETE SET NULL
-)
+);
+
+ALTER TABLE users
+ADD CONSTRAINT fk_user_team
+FOREIGN KEY (team_id) REFERENCES teams(id) ON DELETE SET NULL;
+
+ALTER TABLE teams
+ADD CONSTRAINT fk_team_admin
+FOREIGN KEY (admin_user_id) REFERENCES user(id) ON DELETE SET NULL;
 
 CREATE TABLE IF NOT EXISTS tasks (
     id INT AUTO_INCREMENT PRIMARY KEY,
@@ -40,13 +47,14 @@ CREATE TABLE IF NOT EXISTS tasks (
     due_date TIMESTAMP NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     completed_at TIMESTAMP,
-    created_by INT NOT NULL,
-    FOREIGN KEY (created_by) REFERENCES users(id) ON DELETE CASCADE
-)
+    created_by INT,
+    FOREIGN KEY (created_by) REFERENCES users(id) ON DELETE SET NULL
+);
 
 CREATE TABLE IF NOT EXISTS tasks_doers (
     user_id INT NOT NULL,
     task_id INT NOT NULL,
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
-    FOREIGN KEY (task_id) REFERENCES tasks(id) ON DELETE CASCADE
-)
+    FOREIGN KEY (task_id) REFERENCES tasks(id) ON DELETE CASCADE,
+    PRIMARY KEY (user_id, task_id)
+);
