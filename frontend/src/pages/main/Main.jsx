8d0ca592ef1564e950ab6e13b631ad8031a2a25c
Kevin <185861753+Scrasms@users.noreply.github.com>
2025-05-16 12:46:15 +1000

    added create team and join team buttons to main page

--- frontend/src/pages/main/Main.jsx
+++ frontend/src/pages/main/Main.jsx
@@ -1,13 +1,22 @@
 import { useState } from 'react'
+import createTeam from '../../assets/createTeam.png';
+import joinTeam from '../../assets/joinTeam.png';
 import './Main.css'
 
 function Main() {
   const [count, setCount] = useState(0)
 
+  const onClick = () => {
+    console.log('hello world!');
+  }
+
   return (
     <>
       <div>
-        <h1>Main Page</h1>
+        <h1>Welcome!</h1>
+        <h2>Set off on your journey to be the very best...</h2>
+        <img className='create-team-button' src={createTeam} onClick={onClick}/>
+        <img className='join-team-button' src={joinTeam}/>
       </div>
     </>
   )
