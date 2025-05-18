9cbe376e912a1b4ebfd5e3a395be89a68f7a6fc6
Kevin <185861753+Scrasms@users.noreply.github.com>
2025-05-18 16:28:32 +1000

    moved navBar to separate component and fixed styles for it

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,24 +1,13 @@
-import homeBtn from '../../assets/home.png';
-import newTaskBtn from '../../assets/new_task.png';
-import viewTaskBtn from '../../assets/view_task.png';
-import profileBtn from '../../assets/profile.png';
-import arrow from '../../assets/arrow.png';
+import NavBar from './NavBar';
 import './Home.css';
 
 function Home() {
 
     return (
         <>
-            <ul>
-                <li className="up-arrow"><img src={arrow}/></li>
-                <li><img src={homeBtn}/></li>
-                <li><img src={newTaskBtn}/></li>
-                <li><img src={viewTaskBtn}/></li>
-                <li><img src={profileBtn}/></li>
-                <li className="down-arrow"><img src={arrow}/></li>
-            </ul>
+            <NavBar></NavBar>
         </>
-    )
+    );
 }
 
 export default Home
---
+++ frontend/src/pages/home/NavBar.jsx
@@ -0,0 +1,45 @@
+import { useState } from 'react';
+import homeBtn from '../../assets/home.png';
+import newTaskBtn from '../../assets/new_task.png';
+import viewTaskBtn from '../../assets/view_task.png';
+import profileBtn from '../../assets/profile2.png';
+import arrow from '../../assets/arrow.png';
+import './Home.css';
+
+function NavBar() {
+    const [ clicked, setClicked ] = useState('');
+
+    return (
+        <>
+            <ul>
+                <li className="up-arrow"><img src={arrow}/></li>
+                <li>
+                    <img className={clicked == 'home' ? 'clicked' : ''}
+                        src={homeBtn}
+                        onClick={() => setClicked('home')}/>
+                </li>
+                <li>
+                    <img
+                        className={clicked == 'newTask' ? 'clicked' : ''}
+                        src={newTaskBtn}
+                        onClick={() => setClicked('newTask')}/>
+                </li>
+                <li>
+                    <img
+                        className={clicked == 'viewTask' ? 'clicked' : ''}
+                        src={viewTaskBtn}
+                        onClick={() => setClicked('viewTask')}/>
+                </li>
+                <li>
+                    <img
+                        className={clicked == 'profile' ? 'clicked' : ''}
+                        src={profileBtn}
+                        onClick={() => setClicked('profile')}/>
+                </li>
+                <li className="down-arrow"><img src={arrow}/></li>
+            </ul>
+        </>
+    );
+}
+
+export default NavBar;
