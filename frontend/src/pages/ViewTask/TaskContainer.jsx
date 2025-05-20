200afb1d918eb52256ab41f800afe45964e88084
Kevin <185861753+Scrasms@users.noreply.github.com>
2025-05-20 16:30:18 +1000

    added filters to each taskcontainer so they display different tasks

--- frontend/src/pages/ViewTask/TaskContainer.jsx
+++ frontend/src/pages/ViewTask/TaskContainer.jsx
@@ -1,22 +1,19 @@
-import { useState, useEffect, useContext } from 'react';
+import { useState, useEffect } from 'react';
 import Task from '../../components/Task/Task';
 import { getTaskData } from '../../api/tasks';
-import { AuthContext } from "../../context/authContext";
 import './ViewTask.css';
 import Popup from '../../components/Popup';
 import ExpandedTask from '../../components/Task/ExpandedTask';
 
-function TaskContainer({title}) {
+function TaskContainer({title, filter}) {
     const [taskData, setTaskData] = useState([]);
     const [taskActive, setTaskActive] = useState(false);
-    const { user, loading } = useContext(AuthContext);
     const [expandedTask, setExpandedTask] = useState(null);
 
     // Fetch the task information
     useEffect(() => {
         (async () => {
-            // set taskStatus: "pending", incomplete" or "complete" depending on type of viewTask LATER
-            const resData = await getTaskData({assignedTo: user.id});
+            const resData = await getTaskData(filter);
             // If an error happens, resData.data is null so make it an empty array and print error
             setTaskData(resData.data || []);
             if (!resData.data) {
@@ -42,7 +39,7 @@ function TaskContainer({title}) {
         <>
             <div className="task-container">
                 <div className="task-container-title">{title}</div>
-                {taskData.length === 0 ? <span className="done-text">All done!</span> : displayTask()}
+                {taskData.length === 0 ? <span className="empty-text">Nothing here!</span> : displayTask()}
             </div>
 
             <Popup active={taskActive}>
--- frontend/src/pages/ViewTask/ViewTask.jsx
+++ frontend/src/pages/ViewTask/ViewTask.jsx
@@ -1,13 +1,33 @@
-import './ViewTask.css';
+import { useContext } from 'react';
+import { AuthContext } from "../../context/authContext";
 import TaskContainer from './TaskContainer';
+import './ViewTask.css';
 
 function ViewTask() {
+    const { user, loading } = useContext(AuthContext);
+
+    // Only show incomplete tasks assigned to the user
+    const incompleteFilter = {
+        assignedTo: user.id,
+        taskStatus: 'incomplete'
+    };
+
+    // Show all pending tasks, regardless of who it was assigned to
+    const pendingFilter = {
+        taskStatus: 'pending'
+    };
+
+    // Show all completed tasks, regardless of who it was assigned to
+    const completeFilter = {
+        taskStatus: 'complete'
+    };
+
     return (
         <>
             <div className="task-viewer">
-                <TaskContainer title="My Tasks"/>
-                <TaskContainer title="Tasks For Review"/>
-                <TaskContainer title="Completed Tasks"/>
+                <TaskContainer title="My Tasks" filter={incompleteFilter}/>
+                <TaskContainer title="Tasks For Review" filter={pendingFilter}/>
+                <TaskContainer title="Completed Tasks" filter={completeFilter}/>
             </div>
         </>
     );
