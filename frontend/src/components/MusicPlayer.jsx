c2dc06cd93080bb421ab91482cfff75aa7e6c61c
Kevin <185861753+Scrasms@users.noreply.github.com>
2025-05-21 10:59:04 +1000

    replaced background music

--- frontend/src/components/MusicPlayer.jsx
+++ frontend/src/components/MusicPlayer.jsx
@@ -1,36 +1,40 @@
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 import playBtn from '../assets/play.png';
 import plusBtn from '../assets/plus.png';
 import minusBtn from '../assets/minus.png';
-import bgMusic from '../assets/littleroot_town.ogg';
+import bgMusic from '../assets/bg.ogg';
 import './MusicPlayer.css';
 
 function MusicPlayer() {
+    const bgMusicRef = useRef(null);
 
-    const bgMusicRef = useRef(new Audio(bgMusic));
+    useEffect(() => {
+        bgMusicRef.current = new Audio(bgMusic);
+        bgMusicRef.current.volume = 0.3;
+        bgMusicRef.current.loop = true;
+    }, []);
 
     const playMusic = () => {
         if (bgMusicRef.current.paused) {
             bgMusicRef.current.play();
-            bgMusicRef.current.volume = 0.3;
-            bgMusicRef.current.loop = true;
+
         } else {
             bgMusicRef.current.pause();
         }
     };
 
     const increaseVolume = () => {
-        bgMusicRef.current.volume += 0.1;
+        bgMusicRef.current.volume = Math.min(bgMusicRef.current.volume + 0.1, 1);
     }
 
     const decreaseVolume = () => {
-        bgMusicRef.current.volume -= 0.1;
+        bgMusicRef.current.volume = Math.max(bgMusicRef.current.volume - 0.1, 0);
     }
 
     return (
         <>
             <div className="music-container">
-                <div className="music-text">Now Playing: Littleroot Town</div>
+                <div className="music-text">Now Playing: Peak Music</div>
                 <div className="music-controls">
                     <img className="unskip music-btn" src={minusBtn} onClick={decreaseVolume}/>
                     <img className="play music-btn" src={playBtn} onClick={playMusic}/>
