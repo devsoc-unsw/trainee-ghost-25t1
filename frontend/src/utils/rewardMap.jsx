b6aab57ec30928e6252ff29625c6098397a96f43
Kevin <185861753+Scrasms@users.noreply.github.com>
2025-05-21 18:01:34 +1000

    added images to task rewards

---
+++ frontend/src/utils/rewardMap.jsx
@@ -0,0 +1,16 @@
+import * as rewards from '../assets/rewards';
+
+const rewardMap = {
+    1: rewards.Leftovers,
+    2: rewards.BlukBerry,
+    3: rewards.Calcium,
+    4: rewards.FriedFillet,
+    5: rewards.LoveSweet,
+    6: rewards.Ether,
+    7: rewards.HyperPotion,
+    8: rewards.RareCandy,
+    9: rewards.ExpShare,
+    10: rewards.MaxRevive,
+}
+
+export default rewardMap;
