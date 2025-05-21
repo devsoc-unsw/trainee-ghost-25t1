44a18268cd7608f393d5df75bcc1ca6597cfd241
William Millet <57897661+WilliamMillet@users.noreply.github.com>
2025-05-21 13:32:53 +1000

    Fixed some of the old issues

--- frontend/src/components/Task/ExpandedTask.jsx
+++ frontend/src/components/Task/ExpandedTask.jsx
@@ -1,79 +1,61 @@
-import { useContext, useState } from "react";
-import Button from "../Button";
-import TaskText from "./TaskText";
-import { claimTaskCompleted, voteOnCompletion } from "../../api/tasks";
-import "./Task.css";
-import { HomeContext } from "../../context/homeContext";
+import { useState } from 'react';
+import Button from '../Button';
+import TaskText from './TaskText';
+import { claimTaskCompleted, voteOnCompletion } from '../../api/tasks';
+import './Task.css';
 
 // Component representing a Task when it is clicked on and expanded
-function ExpandedTask({ task, setTaskActive, filter }) {
-  const [buttonMsg, setButtonMsg] = useState("");
+function ExpandedTask({task, setTaskActive, filter}) {
+    const [buttonMsg, setButtonMsg] = useState("");
 
-  const { setHomeData, homeData } = useContext(HomeContext);
+    // Depends on if task is assigned, done (but need approval from someone), completed
 
-  // Depends on if task is assigned, done (but need approval from someone), completed
+    // Mark a given task as done by calling the backend
+    const markDone = async (taskId) => {
+        const resData = await claimTaskCompleted(taskId);
+        console.log(resData);
 
-  // Mark a given task as done by calling the backend
-  const markDone = async (taskId) => {
-    const resData = await claimTaskCompleted(taskId);
-    if (resData.success) {
-      setHomeData((prev) => ({
-        ...prev,
-        tasks: prev.tasks.map((task) => (
-          taskId === task.id ? { ...task, task_status: "pending" } : task
-        )),
-      }));
-      setButtonMsg("Task successfully marked as done!");
-    } else {
-      console.error(`Error: ${resData.error}`);
-      setButtonMsg(resData.error || "Something went wrong, please try again");
+        if (resData.success) {
+            setButtonMsg("Task successfully marked as done!");
+        } else {
+            console.error(`Error: ${resData.error}`);
+            setButtonMsg(resData.error || "Something went wrong, please try again");
+        }
     }
-  };
 
-  // Mark a given task as done by calling the backend
-  const approve = async (taskId) => {
-    const resData = await voteOnCompletion(taskId);
-    if (resData.success) {
-      setButtonMsg("Task successfully marked for approval!");
-    } else {
-      console.error(`Error: ${resData.error}`);
-      setButtonMsg(resData.error || "Something went wrong, please try again");
+    // Mark a given task as done by calling the backend
+    const approve = async (taskId) => {
+        const resData = await voteOnCompletion(taskId);
+        console.log(resData);
+
+        if (resData.success) {
+            setButtonMsg("Task successfully marked for approval!");
+        } else {
+            console.error(`Error: ${resData.error}`);
+            setButtonMsg(resData.error || "Something went wrong, please try again");
+        }
     }
-  };
 
-  return (
-    <>
-      <div className="background-mask" onClick={() => setTaskActive(false)}>
-        <div className="expanded-task" onClick={(e) => e.stopPropagation()}>
-          <TaskText task={task} shouldTruncate={false} />
-          {filter.taskStatus === "incomplete" && (
-            <Button
-              className="mark-done-btn"
-              onClick={() => markDone(task.id)}
-              topText={buttonMsg}
-              innerText="Mark Done"
-            />
-          )}
-          {filter.taskStatus === "pending" && (
-            <Button
-              className="mark-done-btn"
-              onClick={() => approve(task.id)}
-              topText={buttonMsg}
-              innerText="Approve"
-            />
-          )}
-          {/* If we have time turn this into a FUN Button -> It does absolutely nothing but maybe can make it shoot confetti?? */}
-          {filter.taskStatus === "complete" && (
-            <Button
-              className="mark-done-btn"
-              topText={buttonMsg}
-              innerText="N/A"
-            />
-          )}
-        </div>
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div className="background-mask" onClick={() => setTaskActive(false)}>
+                <div className="expanded-task" onClick={(e) => e.stopPropagation()}>
+                    <TaskText task={task} shouldTruncate={false}/>
+                    {filter.taskStatus === 'incomplete' && (
+                        <Button className="mark-done-btn" onClick={() => markDone(task.id)} topText={buttonMsg} innerText="Mark Done"/>
+                    )}
+                    {filter.taskStatus === 'pending' && (
+                        <Button className="mark-done-btn" onClick={() => approve(task.id)} topText={buttonMsg} innerText="Approve"/>
+                    )}
+                    {/* If we have time turn this into a FUN Button -> It does absolutely nothing but maybe can make it shoot confetti?? */}
+                    {filter.taskStatus === 'complete' && (
+                        <Button className="mark-done-btn" topText={buttonMsg} innerText="N/A"/>
+                    )}
+
+                </div>
+            </div>
+        </>
+    );
 }
 
 export default ExpandedTask;
--- frontend/src/pages/ViewTask/TaskContainer.jsx
+++ frontend/src/pages/ViewTask/TaskContainer.jsx
@@ -1,73 +1,52 @@
-import { useState, useEffect, useContext } from "react";
-import Task from "../../components/Task/Task";
-// import { getTaskData } from "../../api/tasks";
-import "./ViewTask.css";
-import Popup from "../../components/Popup";
-import ExpandedTask from "../../components/Task/ExpandedTask";
-import { HomeContext } from "../../context/homeContext";
+import { useState, useEffect } from 'react';
+import Task from '../../components/Task/Task';
+import { getTaskData } from '../../api/tasks';
+import './ViewTask.css';
+import Popup from '../../components/Popup';
+import ExpandedTask from '../../components/Task/ExpandedTask';
 
-function TaskContainer({ title, filter }) {
-  // const [taskData, setTaskData] = useState([]);
-  const [taskActive, setTaskActive] = useState(false);
-  const [expandedTask, setExpandedTask] = useState(null);
-  console.log(filter);
+function TaskContainer({title, filter}) {
+    const [taskData, setTaskData] = useState([]);
+    const [taskActive, setTaskActive] = useState(false);
+    const [expandedTask, setExpandedTask] = useState(null);
 
-  const { homeData } = useContext(HomeContext);
-  const tasks =
-    homeData?.tasks?.filter((task) => task?.task_status === filter.taskStatus) ||
-    null;
+    // Fetch the task information
+    useEffect(() => {
+        (async () => {
+            const resData = await getTaskData(filter);
+            // If an error happens, resData.data is null so make it an empty array and print error
+            setTaskData(resData.data || []);
+            if (!resData.data) {
+                console.error(resData);
+            }
+        })();
+    }, []);
 
-    console.log(homeData)
+    // When a task is clicked, expand it
+    const handleTaskClick = (task) => {
+        setTaskActive(true);
+        setExpandedTask(task);
+    }
 
-  // Fetch the task information
-  // useEffect(() => {
-  //     (async () => {
-  //         const resData = await getTaskData(filter);
-  //         // If an error happens, resData.data is null so make it an empty array and print error
-  //         setTaskData(resData.data || []);
-  //         if (!resData.data) {
-  //             console.error(resData);
-  //         }
-  //     })();
-  // }, []);
+    // Go through all the fetched tasks and format them as task components
+    const displayTask = () => {
+        return taskData.map((task, index) => {
+            return <Task key={index} task={task} onClick={() => handleTaskClick(task)}/>
+        });
+    }
 
-  // When a task is clicked, expand it
-  const handleTaskClick = (task) => {
-    setTaskActive(true);
-    setExpandedTask(task);
-  };
+    return (
+        <>
+            <div className="task-container">
+                <div className="task-container-title">{title}</div>
+                {taskData.length === 0 ? <span className="empty-text">Nothing here!</span> : displayTask()}
+            </div>
 
-  console.log(homeData.tasks);
-
-  // Go through all the fetched tasks and format them as task components
-  const displayTask = () => {
-    return homeData?.tasks?.map((task, index) => {
-      return (
-        <Task key={index} task={task} onClick={() => handleTaskClick(task)} />
-      );
-    });
-  };
-
-  return (
-    <>
-      <div className="task-container">
-        <div className="task-container-title">{title}</div>
-        {tasks.length === 0 ? (
-          <span className="empty-text">Nothing here!</span>
-        ) : (
-          displayTask()
-        )}
-      </div>
-
-      <Popup active={taskActive}>
-        <ExpandedTask
-          task={expandedTask}
-          setTaskActive={setTaskActive}
-          filter={filter}
-        />
-      </Popup>
-    </>
-  );
+            <Popup active={taskActive}>
+                <ExpandedTask task={expandedTask} setTaskActive={setTaskActive} filter={filter}/>
+            </Popup>
+        </>
+    );
 }
 
 export default TaskContainer;
