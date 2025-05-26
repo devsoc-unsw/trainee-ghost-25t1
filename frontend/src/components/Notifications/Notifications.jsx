541ee16ea6094a73a9fdd91438e75147ad6fd982
Gauri Abeysinghe <g.abeysinghe@student.unsw.edu.au>
2025-05-26 12:50:36 +1000

    fixed bug

--- frontend/src/components/Notifications/Notifications.jsx
+++ frontend/src/components/Notifications/Notifications.jsx
@@ -2,59 +2,86 @@ import "./Notifications.css";
 import badge from "../../assets/badge.png";
 import vote from "../../assets/vote.png";
 import help from "../../assets/help.png";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { HomeContext } from "../../context/HomeContext";
+import Popup from "../Popup";
+import ExpandedTask from "../Task/ExpandedTask";
 // import mewfloat from "../../assets/mewfloat.gif";
 
 // Component that displays a completed task summary
 // Input struct of fields: (key:string)
-function Notifications({setClicked}) {
+function Notifications() {
   const { homeData } = useContext(HomeContext);
+  
+  const [taskActive, setTaskActive] = useState(false);
 
-  console.log(homeData);
-  const task = homeData?.notifications?.[0] || null;
+  const [expandedTask, setExpandedTask] = useState(null);
+  const handleTaskClick = (task) => {
+    setTaskActive(true);
+    setExpandedTask(task);
+  };
+
+  const task = homeData?.notifications || null;
 
   if (task) {
     if (task.type === 'completed') {
       return (
-        <section className="completed-summary-box" onClick={() => setClicked("viewTask")}>
-          <>
-            <div className="summary-image">
-              <img src={badge} />
-            </div>
-            <div className="text-div">
-              <p className="title">{`${task.asignees} completed a task!`}</p>
-              <p className="details">Tap to see Details</p>
-            </div>
-          </>
+        <section className="notifications-section">
+          <section className="completed-summary-box" onClick={() => handleTaskClick(task)}>
+            <>
+              <div className="summary-image">
+                <img src={badge} />
+              </div>
+              <div className="text-div">
+                <p className="title">{`${task.asignees} completed a task!`}</p>
+                <p className="details">Tap to see Details</p>
+              </div>
+            </>
+          </section>
+
+          <Popup active={taskActive}>
+            <ExpandedTask task={expandedTask} setTaskActive={setTaskActive} filter = {{taskStatus: 'complete'}}/>
+          </Popup>
         </section>
       );
     } else if (task.type === 'pending') {
       return (
-        <section className="pending-summary-box" onClick={() => setClicked("viewTask")}>
-          <>
-            <div className="summary-image">
-              <img src={vote} />
-            </div>
-            <div className="text-div">
-              <p className="title">{`${task.asignees} needs you to approve a task for completion`}</p>
-              <p className="details">Tap to see Details</p>
-            </div>
-          </>
+        <section className="notifications-section">
+          <section className="pending-summary-box" onClick={() => handleTaskClick(task)}>
+            <>
+              <div className="summary-image">
+                <img src={vote} />
+              </div>
+              <div className="text-div">
+                <p className="title">{`${task.asignees} needs you to approve a task for completion`}</p>
+                <p className="details">Tap to see Details</p>
+              </div>
+            </>
+          </section>
+
+          <Popup active={taskActive}>
+            <ExpandedTask task={expandedTask} setTaskActive={setTaskActive} filter = {{taskStatus: 'complete'}}/>
+          </Popup>
         </section>
       );
     } else if (task.type === 'overdue') {
       return (
-        <section className="overdue-summary-box" onClick={() => setClicked("viewTask")}>
-          <>
-            <div className="summary-image">
-              <img src={help} />
-            </div>
-            <div className="text-div">
-              <p className="title">{`${task.asignees} might need your help finishing a task...`}</p>
-              <p className="details">Tap to see Details</p>
-            </div>
-          </>
+        <section className="notifications-section">
+          <section className="overdue-summary-box" onClick={() => handleTaskClick(task)}>
+            <>
+              <div className="summary-image">
+                <img src={help} />
+              </div>
+              <div className="text-div">
+                <p className="title">{`${task.asignees} might need your help finishing a task...`}</p>
+                <p className="details">Tap to see Details</p>
+              </div>
+            </>
+          </section>
+
+          <Popup active={taskActive}>
+            <ExpandedTask task={expandedTask} setTaskActive={setTaskActive} filter = {{taskStatus: 'complete'}}/>
+          </Popup>
         </section>
       );
     };
